fcb607da03e6fac280eda14022d62e65a4494cf3
leejabba <dlgodrhs@gmail.com>
2017-11-23 16:03:00 +0900

    템플릿 소스를 이용한 전체 통일화 하기

--- test/bookmarks/urls.py
+++ test/bookmarks/urls.py
@@ -4,12 +4,13 @@ from django.contrib import admin
 
 # 뷰 모듈 관련 클래스 임포트
 from booktag.views import BooktagListView, BooktagDetailView
+from bookmarks.views import HomeView
 
 # url(regex, view, kwargs=None, name=None, prefix='')
 # 보통 regex(정규식), view(뷰), name(URL 패턴 이름) 인자를 사용하는 편이다.
 urlpatterns = [
     # /
-    url(r'^/$', HomeView.as_view(), name="home"),
+    url(r'^$', HomeView.as_view(), name="home"),
 
     # /admin/
     url(r'^admin/', admin.site.urls),   
